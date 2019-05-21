db0b293042d2d87c3cd2ca22d702d24510c2517f
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-21 04:08:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,15 +2,31 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 let neighborhoodId = 0;
+
 class Neighborhood {
   constructor(name) {
     this.name = name;
     this.id = ++neighborhoodId;
+    this
   }
   
   deliveries () {
     return store.deliveries.filter(delivery => {
-      return 
+      return delivery.neighborhoodId === this.id;
     });
+  }
+  
+  meals() {
+    
+  }
+}
+
+let customerID = 0;
+
+class Customer {
+  constructor(name, neighborhood) {
+    this.name = name;
+    this.neighborhoodId = neighborhood;
+    this.id = ++customerID;
   }
 }
